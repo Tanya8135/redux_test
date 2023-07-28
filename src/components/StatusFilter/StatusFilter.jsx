bb81a3c787ce9089ddf02357517754fdab87db6a
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-07-28 11:50:37 +0300

    Add: changed StatusFilter: add- statusFilter, getStatusFilter. Add: (f: redux): constants.js, selectors.js
    useSelector(selector) - використовується для отримання даних зі стору. В аргументі примає ф-ію, яка оголошує параметр state (весь об'єкт стану Redux). Автоматично передає дані ф-ії (або те що нам необхідно)
    
    constants.js (зберігається об'єкт для filter)
    selector.js (для створення ф-ій-селекторівб щоб структурувати код, уникнути дублювання в компонентах)

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,13 +1,18 @@
+import { useSelector } from 'react-redux';
+
 import Button from 'components/Button';
+import { statusFilter } from 'redux/constants';
+import { getStatusFilter } from 'redux/selectors';
 
 import css from './StatusFilter.module.css';
 
 const StatusFilter = () => {
+  const filter = useSelector(getStatusFilter);
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilter.all}>All</Button>
+      <Button selected={filter === statusFilter.active}>Active</Button>
+      <Button selected={filter === statusFilter.completed}>Completed</Button>
     </div>
   );
 };
---
+++ src/redux/constants.js
@@ -0,0 +1,5 @@
+export const statusFilter = {
+    all: 'all',
+    active: 'active',
+    completed: 'completed',
+}
